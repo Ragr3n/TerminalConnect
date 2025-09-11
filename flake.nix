1f12c8d7870cbb8569e6c5e1e5671456f5139298
Robin Ågren <robin@ragren.com>
2025-09-11 10:05:20 +0200

    4

--- flake.nix
+++ flake.nix
@@ -5,17 +5,25 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      packages.${system}.vscode-terminal-connect = pkgs.vscode-utils.buildVscodeExtension {
+      packages.${system}.default = pkgs.vscode-utils.buildVscodeExtension {
+        pname = "terminal-connect";
+
         version = "0.0.1";
         src = ./terminal-connect-0.0.1.vsix;
+    unpackCmd = "cp $src .";
+    sourceRoot = ".";
         vscodeExtPublisher = "Ragr3n";
         vscodeExtName = "TerminalConnect";
         vscodeExtUniqueId = "Ragr3n.TerminalConnect";
+        buildPhase = ''
+          runHook preBuild;
+          runHook postBuild;
+        '';
       };
     };
 }

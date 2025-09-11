fce9d6c64d654d9ad6332b27155f855ea7c2d64b
Robin Ågren <robin@ragren.com>
2025-09-11 07:02:23 +0000

    Updated flake

--- flake.nix
+++ flake.nix
@@ -3,16 +3,20 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.vscode-terminal-connect = nixpkgs.lib.makeOverridable (pkgs: pkgs.vscode-extensions.vscode-utils.buildVscodeExtension {
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    vscode-utils = import (nixpkgs + "/pkgs/applications/editors/vscode/extensions/vscode-utils.nix") { inherit pkgs; };
+  in {
+    packages.${system}.vscode-terminal-connect = vscode-utils.buildVscodeExtension {
       pname = "terminal-connect";
       version = "0.0.1";
-      src = ./terminal-connect-0.0.1.vsix;
+      src = ./dist/terminal-connect-0.0.1.vsix;
       meta = {
         description = "Connect via SSH or telnet to hosts defined in a yaml file directly in VSCode";
         license = pkgs.lib.licenses.mit;
         maintainers = [ "Ragr3n" ];
       };
-    }) {};
+    };
   };
 }

3d416660d974259c0de8db003283b069c7d21ded
Robin Ågren <robin@ragren.com>
2025-09-11 21:28:18 +0200

    Updated flake.nix to include a development shell.

--- flake.nix
+++ flake.nix
@@ -9,14 +9,16 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      pname = "terminal-connect";
+      version = "0.0.1";
     in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ pkgs.vsce ];
+      };
       packages.${system}.default = pkgs.vscode-utils.buildVscodeExtension rec{
-        pname = "terminal-connect";
-
-        version = "0.0.1";
-        src = ./terminal-connect-0.0.1.vsix;
-    # unpackCmd = "cp $src .";
-    # sourceRoot = ".";
+        buildInputs = [ pkgs.nodejs pkgs.vsce ];
+        inherit pname version;
+        src = ./${pname}-${version}.vsix;
         vscodeExtPublisher = "Ragr3n";
         vscodeExtName = "TerminalConnect";
         vscodeExtUniqueId = "Ragr3n.TerminalConnect";
